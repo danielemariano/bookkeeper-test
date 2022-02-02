f73f1e6b7a927f9625edfb6a0fabbd6376f55c22
Daniele Mariano <danielemariano4@gmail.com>
2022-02-02 17:43:58 +0100

    Tests fixed

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/MyReadCacheTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/MyReadCacheTest.java
@@ -27,9 +27,9 @@ public class MyReadCacheTest {
 	@Parameters
 	public static Collection<Object[]> configure(){
        return Arrays.asList(new Object[][] {
-			{UnpooledByteBufAllocator.DEFAULT, 10*1024,1024, 2*1024},
-				{UnpooledByteBufAllocator.DEFAULT, 10*512,512, 2*512}, 
-				{UnpooledByteBufAllocator.DEFAULT, 10*2048,2048, 2*2048}
+			{UnpooledByteBufAllocator.DEFAULT, 10*1024, 1024, 2*1024},
+				{UnpooledByteBufAllocator.DEFAULT, 10*512, 512, 2*512},
+				{UnpooledByteBufAllocator.DEFAULT, 10*2048, 2048, 2*2048}
 		});
 	}
 	
@@ -41,81 +41,91 @@ public class MyReadCacheTest {
     }
     
     @Test
-    public void checkEmptyAndReadingCacheTestSingleSegment() {   	
+    public void checkEmptyAndReadingCacheSingleSegmentTest() {   	
  
-    	System.out.println("checkEmptyAndReadingCacheTestSingleSegment");
-        ReadCache cache = new ReadCache(allocator, maxCacheSize);
+    	System.out.println("checkEmptyAndReadingCacheSingleSegmentTest");
+        
+    	// Inizializzazione della cache utilizzando la ReadCache con gli input 
+    	// presi dai parametri del test definiti nel metodo configure() che essendo
+    	// taggato con @Parameters i suoi parametri vengono utilizzati per ogni run di test
+    	ReadCache cache = new ReadCache(allocator, maxCacheSize);
 
+    	// Test della cache vuota utilizzando e quindi testando il corretto funzionamento
+    	// del metodo count, size e get della classe ReadCache
         System.out.println("Check empty " + maxCacheSize + " cache (created for reading) test");
         assertEquals(0, cache.count());
         assertEquals(0, cache.size());
         assertEquals(null, cache.get(0, 0));
 
+        // Test dell'inserimento di dati nella cache utilizzando il metodo put, size,
+        // count della classe ReadCache sotto test
         System.out.println("Read " + maxCacheSize + " cache test");
         ByteBuf entry = Unpooled.wrappedBuffer(new byte[bufferSize]);
         cache.put(1, 0, entry);
         assertEquals(1, cache.count());
         assertEquals(bufferSize, cache.size());
-
+        
+        // Il test di scrittura viene controllato attraverso il metodo get con il quale
+        // eseguiamo il retrieving delle informazioni appena scritte
         assertEquals(entry, cache.get(1, 0));
         assertNull(cache.get(1, 1));
 
+        // Eseguiamo nuovamente le operazioni attraverso un ciclo di 10 iterazioni 
+        // ed eseguiamo i controlli come in precedenza
         for (int i = 1; i < 10; i++) {
             cache.put(1, i, entry);
         }
-
         assertEquals(10, cache.count());
         assertEquals(10 * bufferSize, cache.size());
-
         cache.put(1, 10, entry);
-
-        // First half of entries will have been evicted
         for (int i = 0; i < 5; i++) {
             assertNull(cache.get(1, i));
         }
-
         for (int i = 5; i < 11; i++) {
             assertEquals(entry, cache.get(1, i));
         }
-
+        
+        // Si utilizza il metodo close per rialasciare le risorse della cache
         cache.close();
     }
     
     @Test
-    public void checkEmptyAndReadingCacheTestMultiSegment() {
+    public void checkEmptyAndReadingCacheMultiSegmentTest() {
 
-    	System.out.println("checkEmptyAndReadingCacheTestMultiSegment");
+    	System.out.println("checkEmptyAndReadingCacheMultiSegmentTest");
+        
+    	// Inizializzazione della cache attraverso la ReadCache con gli input 
+    	// presi dai parametri del test definiti nel metodo configure() che essendo
+    	// taggato con @Parameters viene utilizzato per ogni run di test
         ReadCache cache = new ReadCache(allocator, maxCacheSize, segmentSize);
         
+        // Test della cache vuota
         System.out.println("Check empty " + maxCacheSize + " cache (created for reading) test");
         assertEquals(0, cache.count());
         assertEquals(0, cache.size());
         assertEquals(null, cache.get(0, 0));
         
+        // Eseguo ora un test di scrittura su un esempio di cache multilivello sempre 
+        // utilizzando la put in combinazione con la get
         for (int i = 0; i < 10; i++) {
             ByteBuf entry = Unpooled.wrappedBuffer(new byte[bufferSize]);
             entry.setInt(0, i);
             cache.put(1, i, entry);
         }
-
         for (int i = 0; i < 10; i++) {
             ByteBuf res = cache.get(1, i);
             assertEquals(1, res.refCnt());
-
             assertEquals(bufferSize, res.readableBytes());
             assertEquals(i, res.getInt(0));
         }
-
         assertEquals(10, cache.count());
         assertEquals(10 * bufferSize, cache.size());
-
-        // Putting one more entry, should trigger the 1st segment rollover
         ByteBuf entry = Unpooled.wrappedBuffer(new byte[bufferSize]);
         cache.put(2, 0, entry);
-
         assertEquals(9, cache.count());
         assertEquals(9 * bufferSize, cache.size());
 
+        //Si utilizza il metodo close per rialasciare le risorse della cache
         cache.close();
 
     }
--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/MyWriteCacheTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/MyWriteCacheTest.java
@@ -26,11 +26,6 @@ public class MyWriteCacheTest {
 
     @Parameters
 	public static Collection<Object[]> configure(){
-        // Scegliamo i parametri per testare il metodo in test, vogliamo andare a testare i
-        // metodi put e clear oltre al costruttore questi prendono come argomenti solamente
-        // un long e un allocator, (il metodo put prende anche due id che metterò hardcoded)
-        // ho deciso di aggiungere l'atttributo bufferSize per vedere se aggiungendo entry
-        // di dimensione diversa da quella della cache ci sarebbero stati problemi
 		return Arrays.asList(new Object[][] {
 			{UnpooledByteBufAllocator.DEFAULT, 10*1024,1024},
 				{UnpooledByteBufAllocator.DEFAULT, 1000 * 1024,1024},
@@ -46,33 +41,43 @@ public class MyWriteCacheTest {
     }
 
     @Test
-    public void checkEmptyAndWriteCacheTestSingleSegment() throws Exception {
+    public void checkEmptyAndWriteCacheSingleSegmentTest() throws Exception {
         
-    	System.out.println("checkEmptyAndWriteCacheTestSingleSegment");
+    	System.out.println("checkEmptyAndWriteCacheSingleSegmentTest");
+    	
+    	// Inizializzazione della cache utilizzando la WriteCache con gli input 
+    	// presi dai parametri del test definiti nel metodo configure() che essendo
+    	// taggato con @Parameters i suoi parametri vengono utilizzati per ogni run di test
     	WriteCache cache = new WriteCache(allocator, maxCacheSize, bufferSize);
         
+        // Test della cache vuota utilizzando e quindi testando il corretto funzionamento
+    	// del metodo isEmpty, count e size della classe under test, la WriteCache
     	System.out.println("Check empty " + maxCacheSize + " cache (created for writing) test");
         assertTrue(cache.isEmpty());
         assertEquals(0, cache.count());
         assertEquals(0, cache.size());
         
+        // Test dell'inserimento di dati nella cache utilizzando i metodi
+        // put, size, count della classe WriteCache 
     	System.out.println("Check writing " + maxCacheSize + " cache test");
-        ByteBuf entry1 = allocator.buffer(bufferSize);
-        ByteBufUtil.writeUtf8(entry1, "entry-1");
-        entry1.writerIndex(entry1.capacity());
-        cache.put(1, 1, entry1);
+        ByteBuf entry = allocator.buffer(bufferSize);
+        ByteBufUtil.writeUtf8(entry, "entry-1");
+        entry.writerIndex(entry.capacity());
+        cache.put(1, 1, entry);
         assertFalse(cache.isEmpty());
         assertEquals(1, cache.count());
-        assertEquals(entry1.readableBytes(), cache.size());
-        assertEquals(entry1, cache.getLastEntry(1));
+        assertEquals(entry.readableBytes(), cache.size());
+        assertEquals(entry, cache.getLastEntry(1));
         assertEquals(null, cache.getLastEntry(2));
         
+        // Si utilizza il metodo clear per rialasciare le risorse della cache e
+        // i metodi isEmpty, count e size per controllare la corretta esecuzione
     	System.out.println("Check clearing " + maxCacheSize + " cache writed test");
         cache.clear();
         assertTrue(cache.isEmpty());
         assertEquals(0, cache.count());
         assertEquals(0, cache.size());
-        entry1.release();
+        entry.release();
         cache.close();     
     }
     
@@ -80,8 +85,16 @@ public class MyWriteCacheTest {
     public void fillCacheFullTest() throws Exception {
     	
     	System.out.println("fillCacheFullTest");
+    	
+    	// Inizializzazione della cache utilizzando la WriteCache con gli input 
+    	// presi dai parametri del test definiti nel metodo configure() che essendo
+    	// taggato con @Parameters i suoi parametri vengono utilizzati per ogni run di test
         WriteCache cache = new WriteCache(allocator, maxCacheSize,bufferSize);
     	
+        
+        // Eseguiamo un test di fill completo della cache con le put per poi eseguire i 
+        // controlli isEmpty, count e in questo caso (per completare la copertura della
+        // WriteCache utilizziamo anche il metodo forEach per il controllo della corretta scrittura
         int entriesCount = (int) (maxCacheSize / bufferSize);
         ByteBuf entry = allocator.buffer(bufferSize);
         entry.writerIndex(entry.capacity());
@@ -105,6 +118,8 @@ public class MyWriteCacheTest {
             findCount.incrementAndGet();
         });
         assertEquals(0, findCount.get());
+        
+        //Si utilizza il metodo close per rialasciare le risorse della cache
         entry.release();
         cache.close();
     }
